e7e6d2a79a76bd8fc1dc33cf873b60d82c56d1b8
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-04-07 19:52:59 -0700

    feature: add PersonDAO

---
+++ src/main/java/usa/badratdinova/bookexchange/dao/PersonDAO.java
@@ -0,0 +1,54 @@
+package usa.badratdinova.bookexchange.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import usa.badratdinova.bookexchange.models.Person;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query("SELECT * FROM Person",
+                new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id = ?",
+                new Object[] {id},
+                new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public Optional<Person> show(String surnameNamePatronymic) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE surnameNamePatronymic = ?",
+                new Object[]{surnameNamePatronymic},
+                new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person(surnameNamePatronymic, yearOfBirth) VALUES(?, ?)",
+                person.getSurnameNamePatronymic(), person.getYearOfBirth());
+    }
+
+    public void update(int id, Person person) {
+        jdbcTemplate.update("UPDATE Person SET surnameNamePatronymic = ?, yearOfBirth = ? WHERE id = ?",
+                person.getSurnameNamePatronymic(), person.getYearOfBirth(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id = ?", id);
+    }
+}
