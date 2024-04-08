61ba8a55e91ac765ac22253ccbd9e5c7fe327a55
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-04-07 19:53:41 -0700

    feature: add PersonValidator

---
+++ src/main/java/usa/badratdinova/bookexchange/util/PersonValidator.java
@@ -0,0 +1,35 @@
+package usa.badratdinova.bookexchange.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import usa.badratdinova.bookexchange.dao.PersonDAO;
+import usa.badratdinova.bookexchange.models.Person;
+
+import java.util.Optional;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> oClass) {
+        return Person.class.equals(oClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+        Optional<Person> optionalPersonFromDatabase = personDAO.show(person.getSurnameNamePatronymic());
+        if (optionalPersonFromDatabase.isPresent() && optionalPersonFromDatabase.get().getId() != person.getId()) {
+            errors.rejectValue("surnameNamePatronymic", "", "Person with this name already exists");
+        }
+    }
+}
