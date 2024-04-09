814e93fde5d3c6da50cd7fd80979797fda47b16d
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-04-08 22:01:54 -0700

    feature: add the class BookValidator

---
+++ src/main/java/usa/badratdinova/bookexchange/util/BookValidator.java
@@ -0,0 +1,34 @@
+package usa.badratdinova.bookexchange.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import usa.badratdinova.bookexchange.dao.BookDAO;
+import usa.badratdinova.bookexchange.models.Book;
+
+import java.util.Optional;
+
+@Component
+public class BookValidator implements Validator {
+    private final BookDAO bookDAO;
+
+    @Autowired
+    public BookValidator(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @Override
+    public boolean supports(Class<?> oClass) {
+        return Book.class.equals(oClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Book book = (Book) o;
+        Optional<Book> optionalBookFromDatabase = bookDAO.show(book.getTitle());
+        if (optionalBookFromDatabase.isPresent() && optionalBookFromDatabase.get().getId() != book.getId()) {
+            errors.rejectValue("title", "", "Book with this title already exists");
+        }
+    }
+}
