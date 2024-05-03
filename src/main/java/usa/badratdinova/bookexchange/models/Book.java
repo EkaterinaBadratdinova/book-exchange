7ac0ada1541bb7a8c8bd8ab20aab7aa9be9fc8d5
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-05-02 17:09:02 -0700

    feature: add JPA annotations.

--- src/main/java/usa/badratdinova/bookexchange/models/Book.java
+++ src/main/java/usa/badratdinova/bookexchange/models/Book.java
@@ -1,29 +1,37 @@
 package usa.badratdinova.bookexchange.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
+@Entity
+@Table(name = "Book")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
     @NotNull
+    @Column(name = "title")
     private String title;
 
     @NotNull
+    @Column(name = "author")
     private String author;
 
     @Min(1700)
     @Max(2024)
+    @Column(name = "year")
     private int year;
 
     public Book() {
 
     }
 
-    public Book(int id, String title, String author, int year) {
-        this.id = id;
+    public Book(String title, String author, int year) {
         this.title = title;
         this.author = author;
         this.year = year;
--- src/main/java/usa/badratdinova/bookexchange/models/Person.java
+++ src/main/java/usa/badratdinova/bookexchange/models/Person.java
@@ -1,26 +1,33 @@
 package usa.badratdinova.bookexchange.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 
+@Entity
+@Table(name = "Person")
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
     @NotEmpty(message="This field cannot be empty")
     @Pattern(regexp = "[A-Z]\\w+ [A-Z]\\w+ [A-Z]\\w+",
             message="The field should have the format: Surname Name Patronymic")
+    @Column(name = "surnamenamepatronymic")
     private String surnameNamePatronymic;
 
     @Min(value = 1900, message="Year of birth cannot be earlier than 1900")
     @Max(value = 2024, message = "Year of birth cannot be later than 2024")
+    @Column(name = "yearofbirth")
     private int yearOfBirth;
 
     public Person() {
 
     }
 
-    public Person(int id, String surnameNamePatronymic, int yearOfBirth) {
-        this.id = id;
+    public Person(String surnameNamePatronymic, int yearOfBirth) {
         this.surnameNamePatronymic = surnameNamePatronymic;
         this.yearOfBirth = yearOfBirth;
     }
