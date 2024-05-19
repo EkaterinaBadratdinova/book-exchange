c79fe6781a755505215735ab2d191003f7d6fef4
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-05-18 20:33:10 -0700

    feature: add PeopleRepository

---
+++ src/main/java/usa/badratdinova/bookexchange/repositories/PeopleRepository.java
@@ -0,0 +1,19 @@
+package usa.badratdinova.bookexchange.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import usa.badratdinova.bookexchange.models.Book;
+import usa.badratdinova.bookexchange.models.Person;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+    Optional<Person> findBySurnameNamePatronymic(String surnameNamePatronymic);
+
+    @Query("select b from Book b left join fetch b.person p where p.id = ?1")
+    List<Book> findBooksByPersonId(int id);
+}
