b38c8e8c5958fcc3f756b51d17de62cc46bbcf75
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-05-23 19:43:40 -0700

    feature: add 1 new method for searching books by title

--- src/main/java/usa/badratdinova/bookexchange/repositories/BooksRepository.java
+++ src/main/java/usa/badratdinova/bookexchange/repositories/BooksRepository.java
@@ -2,10 +2,12 @@ package usa.badratdinova.bookexchange.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import usa.badratdinova.bookexchange.models.Book;
 import usa.badratdinova.bookexchange.models.Person;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +17,7 @@ public interface BooksRepository extends JpaRepository<Book, Integer> {
 
     @Query("select p from Person p left join fetch p.books b where b.id = ?1")
     Optional<Person> findPersonByBookId(int id);
+
+    @Query("select b from Book b left join fetch b.person where b.title like :title%")
+    List<Book> findBooksByTitle(@Param("title") String title);
 }
