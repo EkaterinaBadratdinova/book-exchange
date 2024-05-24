953cf8d83e9efa4f90e88a8a60e29cd38727fd08
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-05-23 19:41:25 -0700

    feature: add 2 endpoints: search and pagination with sorting

--- src/main/java/usa/badratdinova/bookexchange/controllers/BooksController.java
+++ src/main/java/usa/badratdinova/bookexchange/controllers/BooksController.java
@@ -2,17 +2,22 @@ package usa.badratdinova.bookexchange.controllers;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import usa.badratdinova.bookexchange.dao.BookDAO;
-import usa.badratdinova.bookexchange.dao.PersonDAO;
 import usa.badratdinova.bookexchange.models.Book;
 import usa.badratdinova.bookexchange.models.Person;
 import usa.badratdinova.bookexchange.services.BooksService;
 import usa.badratdinova.bookexchange.services.PeopleService;
 import usa.badratdinova.bookexchange.util.BookValidator;
+
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequestMapping("/books")
@@ -29,13 +34,44 @@ public class BooksController {
         this.bookValidator = bookValidator;
     }
 
+    //    With pagination and sorting
+    @GetMapping("/pages")
+    public String indexPage(@RequestParam int page,
+                            @RequestParam int booksPerPage,
+                            @RequestParam(required = false) Boolean sortByTitle,
+                            Model model) {
+        Pageable pageable;
+        if (Boolean.TRUE.equals(sortByTitle)) {
+            pageable = PageRequest.of(page, booksPerPage, Sort.by("title"));
+        } else {
+            pageable = PageRequest.of(page, booksPerPage);
+        }
+        Page<Book> booksPage = booksService.findAll(pageable);
+        model.addAttribute("booksPage", booksPage);
+        model.addAttribute("currentPage", page);
+        return "books/indexPage";
+    }
+
+    //    Without pagination
     @GetMapping()
     public String index(Model model) {
         model.addAttribute("books", booksService.findAll());
         return "books/index";
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/search")
+    public String search(@RequestParam(required = false) String title,
+                         @RequestParam(required = false) String searched,
+                         Model model) {
+        List<Book> books = Collections.emptyList();
+        if (title != null && !title.isEmpty()) {
+            books = booksService.findBooksByTitle(title);
+        }
+        model.addAttribute("books", books);
+        return "books/search";
+    }
+
+    @GetMapping("/{id}")
     public String show(@PathVariable("id") int id,
                        Model model,
                        @ModelAttribute("personToBeChosen") Person person) {
