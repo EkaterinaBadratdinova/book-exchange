7ab497a4966dbd5ffea415acc7410e5e35268ba5
Ekaterina Badratdinova <ekaterinafeshch@gmail.com>
2024-05-24 21:19:14 -0700

    fix: remove the method findBooksByPersonId()

--- src/main/java/usa/badratdinova/bookexchange/repositories/PeopleRepository.java
+++ src/main/java/usa/badratdinova/bookexchange/repositories/PeopleRepository.java
@@ -19,9 +19,6 @@ public interface PeopleRepository extends JpaRepository<Person, Integer> {
     @Query("select p from Person p left join fetch p.books")
     List<Person> findAllPeopleAndTheirBooks();
 
-    @Query("select b from Book b left join fetch b.person p where p.id = ?1")
-    List<Book> findBooksByPersonId(int id);
-
     @Query("select p from Person p left join fetch p.books where p.surnameNamePatronymic like :name%")
     List<Person> findPeopleByName(String name);
 }
